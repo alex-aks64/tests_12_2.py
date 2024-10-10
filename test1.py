98a7d1e202a65757d3a819563d38b23b9a4e576f
alex-aks64 <165569356+alex-aks64@users.noreply.github.com>
2024-10-10 22:57:58 +0400

    test1.py

--- test1.py
+++ test1.py
@@ -1,6 +1,7 @@
 import unittest
 import runner_and_tournament as Runner
 
+
 class TournamentTest(unittest.TestCase):
     usain: Runner.Runner
     all_results = {}
@@ -11,16 +12,18 @@ class TournamentTest(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        for k, v in cls.all_results.items():
-            for k, v in v.items():
-                print(f'{k}:{v}')
+         for k, v in cls.all_results.items():
+                 print(f'{k}:{v}')
+
+   
+
     def setUp(self):
         self.usain = Runner.Runner("Усэйн", 10)
         self.andrey = Runner.Runner("Андрей", 9)
         self.nick = Runner.Runner("Ник", 3)
 
     def test_tournament_results_usain_nick(self):
-        print('id:',+self.id())
+        print('id:',self.id())
         tournament = Runner.Tournament(90, self.usain, self.nick)
         results = tournament.start()
         self.assertEqual(results[1], self.usain)
@@ -37,6 +40,7 @@ class TournamentTest(unittest.TestCase):
         results = tournament.start()
         self.assertEqual(results[1], self.usain)
         TournamentTest.all_results["Усэйн vs Андрей vs Ник"] = results[1].name
+        
 
 if __name__ == '__main__':
     unittest.main()
